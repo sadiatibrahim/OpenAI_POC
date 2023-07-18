d06a8526eb865895dc87970557fcdbaa3cff7db5
sadiatibrahim <57956925+sadiatibrahim@users.noreply.github.com>
2023-07-17 20:25:21 -0400

    function call

--- functionclassifier.py
+++ functionclassifier.py
@@ -5,11 +5,12 @@ from langchain.chat_models import ChatOpenAI
 import re
 import os
 
+
 OPENAI_API_KEY = os.environ.get("SECRET_KEY")
 
 class FunctionClassifier():
     def __init__(self):
-        with open('functions.json') as file:
+        with open('OpenAI_POC\functionclassifier.py') as file:
             self.data = json.load(file)
         self.llm = ChatOpenAI(openai_api_key= OPENAI_API_KEY, temperature=0.0)
     
@@ -28,23 +29,24 @@ class FunctionClassifier():
                          with a single function's description. if it is found that the query does not match a single function's description,
                          return -1. Otherwise, ensure that the query matches the first of the query that you as an AI can 
                          generate a classification for which query matches the definition. If a match is found, return the index of the function description from the list.Make sure to process\
-                         each decription carefully when classifying which description matches the query""",
+                         each decription carefully when classifying which description matches the query. Return only the index of the function description from the list as an output. Do not give me explanations\.""",
         )
 
         chain = LLMChain(llm=self.llm, prompt=prompt)
         result = chain.run(function_desc= str(self.get_function_descriptions()), query=query)
         print(result)
+
+        last_word = result.split()[-1]
         # The string containing the index value
-        index_string = "index = match_query(Definitions, Query)\nprint(index)  # Output: 0"
+        #pattern = r"The output will be:\s*(\d+)"
 
         # Extract the value using regular expressions
-        match = re.search(r'Output:\s*(\d+)', index_string)
+        #match = re.search(pattern, result)
 
-        if match:
-            output_value = match.group(1)
+        if last_word.endswith('.'):
+            last_word = last_word[:-1]
 
 
-        
-        return [(self.data[int(output_value)])]
+        return [(self.data[int(last_word)])]
     
 
--- main.py
+++ main.py
@@ -5,18 +5,19 @@ import os
 import pytz
 from datetime import datetime
 from functionclassifier import FunctionClassifier
+
 OPENAI_API_KEY = os.environ.get("SECRET_KEY")
 def get_batch_progress(query):
 
     #Assume this process is connecting to API and getting json data
-    with open('summaryAPI.json') as file:
+    with open('OpenAI_POC\summaryAPI.json') as file:
         data = json.load(file)
 
     #call the function that summarizes the data
-    result = get_summary(data, query)
+    result = get_summary_batch_progress(data, query)
     return result
 
-def get_summary(json_data, query):
+def get_summary_batch_progress(json_data, query):
     # Get the current time
     current_time = datetime.now()
 
@@ -36,19 +37,122 @@ def get_summary(json_data, query):
         about the progress of mainframe batch or mainframe batch jobs, your answer should be the summary of only mainframe data."
     
     headers = {'content-type':'application/json', "Authorization": "Bearer" + " " + OPENAI_API_KEY}
+    messages = [{"role": "user", "content": prompt}]
     payload= {
-      "model":"text-davinci-003",
-      "prompt": prompt,
+      "model":"gpt-3.5-turbo-0613",
+      "messages": messages,
       "max_tokens" : 1024,
       "temperature" : 0
     }
 
     try:
-        result = requests.post("https://api.openai.com/v1/completions", headers=headers, data=json.dumps(payload))
+        result = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, data=json.dumps(payload))
     except Exception as e:
         return {'status': -1, 'error': 'Error occured' + str(e)}
     
     return {'status':0, 'result': result.text}
+
+
+def get_business_function(job_name):
+    #Assume this process is connecting to API and getting json data
+    with open('OpenAI_POC\businessfunctionsAPI.json') as file:
+        data = json.load(file)
+
+    #call the function that summarizes the data
+    result = get_summary_business_functions(data, job_name)
+    return result
+
+
+def get_summary_business_functions(json_data, job_name):
+    prompt = f"given this json data: {json_data}, write a summary of this json data in sentences. you can start your sentence by saying\
+          'The business functions that would be impacted if {job_name} is delayed is'. when you are done reading\
+          the business functions, continue your sentence with 'and application impacted are'\
+            then summarize the applicationsImpacted data list in the json.\
+            your summary should be concise and formal."
+    headers = {'content-type':'application/json', "Authorization": "Bearer" + " " + OPENAI_API_KEY}
+    messages = [{"role": "user", "content": prompt}]
+
+    payload = {"model" : "gpt-3.5-turbo-0613",
+               "messages" : messages,
+               "temperature" : 0.0,
+               "top_p" : 1.0,
+               "frequency_penalty": 0.0,
+               "presence_penalty" : 0.0,
+               "max_tokens" : 1000
+    }
+    data = json.dumps(payload).encode('utf-8')
+    try:
+        result = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, data=json.dumps(payload))
+    except Exception as e:
+        return {'status': -1, 'error': 'Error occured' + str(e)}
+    
+    return {'status':0, 'result': result.text}
+    # result = requests.post(url, headers=headers, data=data)
+    # return result.text
+
+
+def get_failed_jobs(query):
+    #Assume this process is connecting to API and getting json data
+    with open('OpenAI_POC\failedjobs.json') as file:
+        data = json.load(file)
+
+    result = get_critical_job_failure(data, query)
+    return result
+
+    
+def get_critical_job_failure(json_data, query):
+    prompt = f"take a look at this json data: {json_data}/n give me the list of jobs that failed. i.e \
+        return the name of the job that has 'failed' as status. for example, if a job name is 'PPEET', and has a status 'failed', your \
+        sentence should be 'Yes, the list of critical jobs that failed are' "
+    
+    headers = {'content-type':'application/json', "Authorization": "Bearer" + " " + OPENAI_API_KEY}
+    messages = [{"role": "user", "content": prompt}]
+
+    payload = {"model" : "gpt-3.5-turbo-0613",
+               "messages" : messages,
+               "temperature" : 0.0,
+               "top_p" : 1.0,
+               "frequency_penalty": 0.0,
+               "presence_penalty" : 0.0,
+               "max_tokens" : 1000
+    }
+    data = json.dumps(payload).encode('utf-8')
+    try:
+        result = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, data=json.dumps(payload))
+    except Exception as e:
+        return {'status': -1, 'error': 'Error occured' + str(e)}
+    
+    return {'status':0, 'result': result.text}
+
+
+def get_reason(job_name):
+    #Assume this process is connecting to API and getting json data
+    with open('OpenAI_POC\reason_failed_job.json') as file:
+        data = json.load(file)
+
+
+    prompt = f"Using this json data: {data}/n look at the key value pairs and return the value of the key 'reason', and start your sentence with \
+        'The reason why {job_name} failed is"
+    
+    headers = {'content-type':'application/json', "Authorization": "Bearer" + " " + OPENAI_API_KEY}
+    messages = [{"role": "user", "content": prompt}]
+
+    payload = {"model" : "gpt-3.5-turbo-0613",
+               "messages" : messages,
+               "temperature" : 0.0,
+               "top_p" : 1.0,
+               "frequency_penalty": 0.0,
+               "presence_penalty" : 0.0,
+               "max_tokens" : 1000
+    }
+    data = json.dumps(payload).encode('utf-8')
+    try:
+        result = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, data=json.dumps(payload))
+    except Exception as e:
+        return {'status': -1, 'error': 'Error occured' + str(e)}
+    
+    return {'status':0, 'result': result.text}
+
 
 def main_function_call(query):
     url = "https://api.openai.com/v1/chat/completions"
@@ -72,6 +176,7 @@ def main_function_call(query):
     data = json.dumps(payload).encode('utf-8')
     resp = requests.post(url, headers=headers, data=data)
     resp_json = json.loads(resp.text)
+    print(resp_json)
 
     return resp_json
 
@@ -87,7 +192,7 @@ function_argument = json.loads(function_call_output['choices'][0]['message']['fu
 result = json.loads(function_name(**function_argument).get("result", 'error'))
 # a = get_data(query_str)
 # print(a)
-resp = result['choices'][0]['text']
+resp = result['choices'][0]['message']['content']
 print(resp)
 
     
